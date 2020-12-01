d3d6a25d4b5a912171788305bf279bc3bd03938d
Luiz Matos <lfmatosmelo@id.uff.br>
2020-12-01 20:29:26 -0300

    feat

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ parser = argparse.ArgumentParser(
     description="PyGMRES - Implementacao de GMRES em Python")
 parser.add_argument("--lsyst", type=str,
                     help="arquivo com o sistema a ser resolvido, formato: Ax=b. O arquivo deve conter A e b", required=True)
+parser.add_argument("--resid", type=float,
+                    help="residuo minimo para parar as iteracoes", required=False, default=1e-8)
+parser.add_argument("--niter", type=int,
+                    help="maximo de iteracoes", required=False, default=50)
 parser.add_argument("--debug", type=int,
                     help="debug ativado ou nao", required=False, default=1)
 args = parser.parse_args()
 
-A, b = read_system_of_lineq_from_file(args.lsyst)
-print(f'A={A}')
-print(f'b={b}')
+debug_activated = bool(args.debug)
 
-x = gmres(A, b)
+A, b = read_system_of_lineq_from_file(args.lsyst)
+if debug_activated:
+    print(f'A={A}')
+    print(f'b={b}')
+
+x = gmres(A, b, args.niter, args.resid)
 print(f'x={x}')
